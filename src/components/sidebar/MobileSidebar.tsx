9a68b6aed2c7833400ed7116bfb67cf227b67b95
Adam Farhan <adamfarhan11@gmail.com>
2024-10-25 14:24:41 -0400

    added sidebar and navbar components

---
+++ src/components/sidebar/MobileSidebar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { MenuIcon } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+import { Sidebar } from "./Sidebar";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+
+export const MobileSidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
+  return (
+    <Sheet modal={false} open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger asChild>
+        <Button variant="ghost" className="lg:hidden">
+          <MenuIcon className="text-neutral-500" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import { DottedSeparator } from "../dotted-separator";
+import { SidebarNavigation } from "./SidebarNavigation";
+
+export const Sidebar = () => {
+  return (
+    <aside className="h-full bg-neutral-100 p-4 w-full">
+      <Link href="/">
+        <Image src="/logo.svg" alt="logo" width={200} height={56} />
+      </Link>
+      <DottedSeparator className="my-4" />
+      <SidebarNavigation />
+    </aside>
+  );
+};
