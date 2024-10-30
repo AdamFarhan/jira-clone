c063dafa9726d61912fba0b417ae524faf74b65e
Adam Farhan <adamfarhan11@gmail.com>
2024-10-30 13:25:16 -0400

    Added landing page for workspaces

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,15 +1,17 @@
-import { getAuthedUser } from "@/features/auth/actions";
-import { CreateWorkspaceForm } from "@/features/workspaces/components/CreateWorkspaceForm";
 import { redirect } from "next/navigation";
+
+import { getAuthedUser } from "@/features/auth/actions";
+import { getWorkspaces } from "@/features/workspaces/actions";
 
 export default async function Home() {
   const user = await getAuthedUser();
 
   if (!user) redirect("/sign-in");
 
-  return (
-    <div className="bg-neutral-500 h-full p-4">
-      <CreateWorkspaceForm />
-    </div>
-  );
+  const workspaces = await getWorkspaces();
+  if (workspaces.total === 0) {
+    redirect("/workspaces/create");
+  } else {
+    redirect(`/workspaces/${workspaces.documents[0].$id}`);
+  }
 }
--- src/components/sidebar/WorkspaceSwitcher.tsx
+++ src/components/sidebar/WorkspaceSwitcher.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { RiAddCircleFill } from "react-icons/ri";
+import { useRouter } from "next/navigation";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/useGetWorkspaces";
 import {
@@ -10,16 +11,23 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { WorkspaceAvatar } from "@/features/workspaces/components/WorkspaceAvatar";
+import { useWorkspaceId } from "@/features/workspaces/hooks/useWorkspaceId";
 
 export const WorkspaceSwitcher = () => {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
   const { data: workspaces } = useGetWorkspaces();
+
+  const onSelect = (id: string) => {
+    router.push(`/workspaces/${id}`);
+  };
   return (
     <div className="flex flex-col gap-y-2">
       <div className="flex items-center justify-between">
         <p className="text-xs uppercase text-neutral-500">Workspaces</p>
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
       </div>
-      <Select>
+      <Select onValueChange={onSelect} value={workspaceId}>
         <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
           <SelectValue placeholder="No workspace selected" />
         </SelectTrigger>
